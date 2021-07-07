82f5c8efa3eae1b6202ca3c3a047f5e886a1f019
Andrew6rant <57331134+Andrew6rant@users.noreply.github.com>
2021-07-07 13:18:07 -0400

    Fabric setup

---
+++ src/main/java/io/github/Andrew6rant/foodexpansion/FoodExpansion.java
@@ -0,0 +1,10 @@
+package io.github.Andrew6rant.foodexpansion;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FoodExpansion implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
