6613901271fbb5ae370a8f4689a3ae5084d165b0
Andrew6rant <57331134+Andrew6rant@users.noreply.github.com>
2021-07-07 14:30:29 -0400

    implemented item registry

--- src/main/java/io/github/Andrew6rant/foodexpansion/FoodExpansion.java
+++ src/main/java/io/github/Andrew6rant/foodexpansion/FoodExpansion.java
@@ -1,10 +1,27 @@
 package io.github.Andrew6rant.foodexpansion;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class FoodExpansion implements ModInitializer {
+
+	public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
+			new Identifier("foodexpansion", "general"),
+			() -> new ItemStack(FoodExpansion.bacon));
+
+	public static void registerItem(String itemName, Item item) {
+		Registry.register(Registry.ITEM, new Identifier("foodexpansion", itemName), item);
+	}
+
+	public static Item bacon = new Item(new Item.Settings().group(FoodExpansion.ITEM_GROUP));
+
 	@Override
 	public void onInitialize() {
-
+		registerItem(ItemNames.BACON, bacon);
 	}
 }
---
+++ src/main/java/io/github/Andrew6rant/foodexpansion/ItemNames.java
@@ -0,0 +1,5 @@
+package io.github.Andrew6rant.foodexpansion;
+
+public class ItemNames {
+    public static final String BACON = "bacon";
+}
